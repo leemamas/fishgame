c8d4e3ecf0aef7cd504043a57225e4312fe2ad31
Leemamas <76854100@163.com>
2021-09-22 13:54:20 +0800

    1.绘制场景

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame,sys
+
+pygame.init()
+
+pygame.display.set_caption('fish')
+
+bg=pygame.image.load('images/bg.jpg')
+bar=pygame.image.load('images/bottom-bar.png')
+
+screen=pygame.display.set_mode((1024,768))
+
+
+def main():
+    while 1:
+
+        screen.blit(bg,(0,0))
+        screen.blit(bar,((bg.get_width()-bar.get_width())/2,bg.get_height()-bar.get_height()))
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
